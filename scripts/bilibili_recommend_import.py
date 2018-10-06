51d131e7c7c63c0beb8fed6d6a71da15e81158ef
Yu Sun <usunyu@gmail.com>
2018-10-06 17:46:38 +0800

    Finish bilibili recommend script

--- scripts/bilibili_recommend_import.py
+++ scripts/bilibili_recommend_import.py
@@ -1,27 +1,36 @@
 # -*- coding: utf-8 -*-
+'''
+Parsing data from http://api.bilibili.cn/recommend and
+creating corresponding PorterJob.
+'''
 import requests, json
-from porter.models import PorterJob
+from porter.models import PorterJob, YoutubeAccount
 
 API_URL = 'http://api.bilibili.cn/recommend'
+VIDEO_URL = 'https://www.bilibili.com/video/av'
 
 print('Fetching data...')
-
 response = requests.get(API_URL)
 payload = json.loads(response.text)
-
 pages = payload['pages']
 num = payload['num']
-
 print('Total pages:' + str(pages) + ', num: ' + str(num))
 
-list = payload['list']
+account = YoutubeAccount.objects.all().first()
+print('Target account: ' + account.name)
 
 print('Parsing data and creating jobs...')
 page = pages
 while page >= 1:
     api_url = '{}?page={}'.format(API_URL, page)
     res = requests.get(API_URL)
-    data = json.loads(res.text)['list']
+    list = json.loads(res.text)['list']
+
+    for record in list:
+        aid = record['aid']
+        # create porter job
+        video_url = '{}{}'.format(VIDEO_URL, aid)
+        PorterJob(video_url=video_url, youtube_account=account).save()
 
     page = page - 1
 
@@ -30,5 +39,4 @@ while page >= 1:
     if progress == pages:
         end = '\r\n'
     print('Progress: {:.0%}'.format(float(progress) / pages), end=end, flush=True),
-
 print('Done!')
