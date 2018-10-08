d10749523adb4d771d21e7b1f344cd302756cc21
Ubuntu <ubuntu@ip-172-31-4-3.us-west-1.compute.internal>
2018-10-08 07:43:21 +0000

    Fix debug & static setting

--- server/settings.py
+++ server/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@@z#iju77mkxd$@=u#mq#^6wa-^^(4_!d2%!qgi@60bk7)-gnl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+# DEBUG = True
+DEBUG = False
 
 # Schedule job switch, turn off before initial migrations
 # START_JOB = False
@@ -129,5 +129,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
