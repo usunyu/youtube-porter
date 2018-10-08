7e98a3ffdbf7bb40e6b23334eb94c49740f5ba16
Yu Sun <usunyu@gmail.com>
2018-10-08 20:39:36 +0800

    Try fix bilibili download connection error

--- porter/downloaders/bilibili_downloader.py
+++ porter/downloaders/bilibili_downloader.py
@@ -11,31 +11,36 @@ BILIBILI_API = 'https://www.kanbilibili.com/api/video/'
 CHUNK_SIZE = 1024
 
 def download(url):
-    headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'}
-    response = requests.get(url, headers=headers, stream=True)
-    total_size = int(response.headers['content-length'])
-    format = response.headers['Content-Type']
-    filename = get_time_str()
-    if format=='video/mp4':
-        filename = filename + '.mp4'
-    else:
-        filename = filename + '.flv'
-    file = open(filename, 'wb')
-    print_log(TAG, 'Start downloading...')
-    download_size = 0
-    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
-        if chunk: # filter out keep-alive new chunks
-            file.write(chunk)
-            # file.flush()
-        download_size = download_size + CHUNK_SIZE
-        end = '\r'
-        if download_size > total_size:
-            download_size = total_size
-            end = '\r\n'
-        print(TAG, 'Download progress: {:.0%}'.format(float(download_size) / total_size), end=end, flush=True),
-    file.close()
-    print_log(TAG, 'Download finish!')
-    return filename
+    try:
+        headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36'}
+        response = requests.get(url, headers=headers, stream=True)
+        total_size = int(response.headers['content-length'])
+        format = response.headers['Content-Type']
+        filename = get_time_str()
+        if format=='video/mp4':
+            filename = filename + '.mp4'
+        else:
+            filename = filename + '.flv'
+        file = open(filename, 'wb')
+        print_log(TAG, 'Start downloading...')
+        download_size = 0
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                file.write(chunk)
+                # file.flush()
+            download_size = download_size + CHUNK_SIZE
+            end = '\r'
+            if download_size > total_size:
+                download_size = total_size
+                end = '\r\n'
+            print(TAG, 'Download progress: {:.0%}'.format(float(download_size) / total_size), end=end, flush=True),
+        file.close()
+        print_log(TAG, 'Download finish!')
+        return filename
+    except Exception as e:
+        print_log(TAG, 'Unknown error during bilibili download!')
+        print(e)
+        return None
 
 def bilibili_download(job):
     video_url = job.video_url
