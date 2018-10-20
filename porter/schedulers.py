ce65b55ee8525b08ed43ac4c72e46748aa5511ff
Yu Sun <usunyu@gmail.com>
2018-10-20 16:51:40 +0800

    [Bug-fix] Fix reset quota job

--- porter/schedulers.py
+++ porter/schedulers.py
@@ -264,8 +264,14 @@ def reset_quota_job():
             continue
         # OPTIMIZE, use last 99 job upload time
         last_success_job = account.porter_jobs.filter(status=PorterStatus.SUCCESS).reverse().first()
-        interval = get_current_time() - last_success_job.upload_at
-        if interval.total_seconds() > YOUTUBE_UPLOAD_TIME_INTERVAL:
+        should_reset = False
+        if last_success_job:
+            interval = get_current_time() - last_success_job.upload_at
+            if interval.total_seconds() > YOUTUBE_UPLOAD_TIME_INTERVAL:
+                should_reset = True
+        else:
+            should_reset = True
+        if should_reset:
             account.upload_quota = YOUTUBE_UPLOAD_QUOTA
             account.save(update_fields=['upload_quota'])
             print_log(TAG, 'Reset quota for account: ' + account.name)
