5ac0631f9314b0e4671e237ea7d8e6fe0f5ea86f
Yu Sun <usunyu@gmail.com>
2018-11-15 15:39:28 -0800

    [Script] Remove some channel videos

--- scripts/stop_porter_jobs.py
+++ scripts/stop_porter_jobs.py
@@ -10,8 +10,10 @@ from porter.enums import PorterStatus
 account = YoutubeAccount.objects.filter(name='yportcomment').first()
 
 jobs = PorterJob.objects.filter(
-    Q(status=PorterStatus.PENDING) &
-    Q(youtube_account=account))
+    # Q(status=PorterStatus.PENDING) &
+    Q(youtube_account=account) &
+    Q(playlist='猫叔说电影')
+    )
 for job in jobs:
     job.status = PorterStatus.STOP
     job.save(update_fields=['status'])
