9488ff5e9ad801515e2ff68915f627deb3710280
Yu Sun <usunyu@gmail.com>
2018-11-29 12:04:41 -0800

    [Optimize] Print fetch last channel log

--- scripts/fetch_last_channel.py
+++ scripts/fetch_last_channel.py
@@ -10,5 +10,6 @@ from porter.fetchers.bilibili_fetcher import bilibili_channel_fetch
 job = ChannelJob.objects.last()
 
 if job:
+    print('Fetching the last added channel jobs {}...'.format(job.name))
     if job.video_source == VideoSource.BILIBILI:
         bilibili_channel_fetch(job)
